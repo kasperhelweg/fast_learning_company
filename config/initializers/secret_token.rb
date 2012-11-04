e324f6212e752bd815a1ac0c1abed366acb3b281
Kasper Helweg Jonassen <kasperhelweg@gmail.com>
2012-11-04 18:58:37 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FastLearningCompany::Application.config.secret_token = 'f6081ee87e010d5b047b0c0563b2bed6fe32c5371280da8d0e6ca7b95dd1dfd04841a2dfeffc5618f0cc960bc97b110297e18e0c7eb7a17005bc35edce51d5d3'
