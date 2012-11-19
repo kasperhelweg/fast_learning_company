5cf1d8b59dac9d8932dae37a6cdfe82429f2f6dd
Kasper Helweg Jonassen <kasperhelweg@gmail.com>
2012-11-19 20:36:55 +0100

    Ready for testing / refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,7 @@ class User < ActiveRecord::Base
 
   has_attached_file :avatar, 
                     :styles => { :medium => "140x140>", :thumb => "35x35>" },
+                    :default_url => '/assets/avatars/default_:style.png',
                     :path => "/avatars/:style/:id/:filename",
                     :s3_permissions => :public_read
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,38 +6,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-# Seed users
-user_comp_1 = Company.new( 
-                          name: "Helweg & Pedersen",
-                          email: "info@helweg-pedersen.dk", 
-                          password: "123456", 
-                          password_confirmation: "123456" )
-
-user_comp_2 = Company.new( 
-                          name: "Some Company",
-                          email: "info@somecompany.com", 
-                          password: "123456", 
-                          password_confirmation: "123456" )
-
-user_emp_1 = user_comp_1.learners.build(
-                                        name: "Stinne Tarp",
-                                        email: "stinne@helweg-pedersen.dk", 
-                                        password: "123456", 
-                                        password_confirmation: "123456" )
-
-user_emp_2 = user_comp_1.learners.build(
-                                        name: "Kasper Helweg",
-                                        email: "kasperhelweg@gmail.com", 
-                                        password: "123456", 
-                                        password_confirmation: "123456" )
-
-
-#user_emp_2.avatar = File.open("/home/kasper/Pictures/kaps.png")
-
-
-user_comp_1.save!
-user_comp_2.save!
+require File.expand_path('../seeds/users', __FILE__)
 
 # Seed courses
 course_MUS = Course.new(
